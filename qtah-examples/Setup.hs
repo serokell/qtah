516764fe1fcabcc5d0845c70b079182c205b2252
Bryan Gardiner <bog@khumba.net>
2017-01-11 00:18:15 -0800

    Make qtah-examples warn when not building dynamically (closes #15).

--- qtah-examples/Setup.hs
+++ qtah-examples/Setup.hs
@@ -1,6 +1,6 @@
 -- This file is part of Qtah.
 --
--- Copyright 2016 Bryan Gardiner <bog@khumba.net>
+-- Copyright 2016-2017 Bryan Gardiner <bog@khumba.net>
 --
 -- This program is free software: you can redistribute it and/or modify
 -- it under the terms of the GNU Lesser General Public License as published by
@@ -15,7 +15,25 @@
 -- You should have received a copy of the GNU Lesser General Public License
 -- along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import Distribution.Simple (defaultMain)
+import Control.Monad (unless)
+import Distribution.Simple (defaultMainWithHooks, simpleUserHooks)
+import Distribution.Simple.Setup (ConfigFlags, configDynExe, configVerbosity, fromFlagOrDefault)
+import Distribution.Simple.UserHooks (UserHooks (postConf))
+import Distribution.Simple.Utils (warn)
+import Distribution.Verbosity (normal)
 
 main :: IO ()
-main = defaultMain
+main = defaultMainWithHooks qtahHooks
+
+qtahHooks :: UserHooks
+qtahHooks = simpleUserHooks
+  { postConf = \_ cf _ _ -> warnAboutDynExe cf
+  }
+
+warnAboutDynExe :: ConfigFlags -> IO ()
+warnAboutDynExe configFlags = do
+  let verbosity = fromFlagOrDefault normal $ configVerbosity configFlags
+  unless (fromFlagOrDefault False $ configDynExe configFlags) $
+    warn verbosity $
+    "qtah-examples needs to be a dynamically linked executable; " ++
+    "please pass --enable-executable-dynamic to 'cabal install'."
