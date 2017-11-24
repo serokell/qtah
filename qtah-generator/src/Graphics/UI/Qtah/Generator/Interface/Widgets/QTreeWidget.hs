a82ed92d39e2e06af9b26a278ead52fd85b107ce
Yuriy Syrovetskiy <cblp@cblp.su>
2017-11-24 19:09:09 +0300

    Add QTreeView, QTreeWidget

---
+++ qtah-generator/src/Graphics/UI/Qtah/Generator/Interface/Widgets/QTreeWidget.hs
@@ -0,0 +1,69 @@
+-- This file is part of Qtah.
+--
+-- Copyright 2015-2017 The Qtah Authors.
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU Lesser General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU Lesser General Public License for more details.
+--
+-- You should have received a copy of the GNU Lesser General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+module Graphics.UI.Qtah.Generator.Interface.Widgets.QTreeWidget (
+  aModule,
+  ) where
+
+import Foreign.Hoppy.Generator.Spec (
+  Export (ExportClass, ExportEnum),
+  addReqIncludes,
+  classSetEntityPrefix,
+  ident,
+  ident1,
+  includeStd,
+  makeClass,
+  mkBoolIsProp,
+  mkConstMethod,
+  mkCtor,
+  mkMethod,
+  mkMethod',
+  mkProp,
+  )
+import Foreign.Hoppy.Generator.Types (boolT, enumT, intT, objT, ptrT, voidT)
+import Foreign.Hoppy.Generator.Version (collect, just, test)
+import Graphics.UI.Qtah.Generator.Flags (qtVersion)
+import Graphics.UI.Qtah.Generator.Interface.Core.QSize (c_QSize)
+import Graphics.UI.Qtah.Generator.Interface.Core.QString (c_QString)
+import Graphics.UI.Qtah.Generator.Interface.Core.Types (e_Corner, e_TextElideMode)
+import Graphics.UI.Qtah.Generator.Interface.Gui.QIcon (c_QIcon)
+import Graphics.UI.Qtah.Generator.Interface.Internal.Listener (c_ListenerInt)
+import Graphics.UI.Qtah.Generator.Interface.Widgets.QTreeView (c_QTreeView)
+import Graphics.UI.Qtah.Generator.Interface.Widgets.QWidget (c_QWidget)
+import Graphics.UI.Qtah.Generator.Module (AModule (AQtModule), makeQtModule)
+import Graphics.UI.Qtah.Generator.Types
+
+{-# ANN module "HLint: ignore Use camelCase" #-}
+
+aModule =
+  AQtModule $
+  makeQtModule ["Widgets", "QTreeWidget"] $
+  QtExport (ExportClass c_QTreeWidget) :
+  map QtExportSignal signals
+
+c_QTreeWidget =
+  addReqIncludes [includeStd "QTreeWidget"] $
+  classSetEntityPrefix "" $
+  makeClass (ident "QTreeWidget") Nothing [c_QTreeView] $
+  collect
+  [ just $ mkCtor "new" []
+    -- TODO
+  ]
+
+signals =
+  [ makeSignal c_QTreeWidget "currentChanged" c_ListenerInt
+  ]
