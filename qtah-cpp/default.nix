87748638d8a06efdeb86325706713161ea5fe205
Bryan Gardiner <bog@khumba.net>
2018-09-06 20:29:21 -0700

    Bump qtah-cpp-0.5.1 and update the changelog.

--- qtah-cpp/default.nix
+++ qtah-cpp/default.nix
@@ -21,7 +21,7 @@
 }:
 mkDerivation ({
   pname = "qtah-cpp";
-  version = "0.5.0";
+  version = "0.5.1";
   src = ./.;
   libraryHaskellDepends = [ base Cabal qtah-generator ];
   librarySystemDepends = [ qt ];
